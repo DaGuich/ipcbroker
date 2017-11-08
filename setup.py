36a20b2540b0ec653362e783693ef3460d5cd567
Matthias Gilch <matthias.gilch.mg@gmail.com>
2017-11-08 21:22:27 +0100

    Configuration for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='ipcbroker',
+    packages=['ipcbroker'],
+    version='0.1',
+    description='Interprocess communication framework',
+    author='Matthias Gilch',
+    author_email='matthias.gilch.mg@gmail.com',
+    url='https://github.com/DaGuich/ipcbroker',
+    download_url='https://github.com/DaGuich/ipcbroker/archive/0.1.tar.gz',
+    keywords=['IPC', 'interprocess', 'communication', 'broker', 'client']
+)
