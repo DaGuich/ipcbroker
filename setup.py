400259308ac386906768554523e2cc4d75aa2e14
Matthias Gilch <matthias.gilch.mg@gmail.com>
2017-11-14 11:28:58 +0100

    Introduced new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='ipcbroker',
     packages=['ipcbroker'],
-    version='0.1',
+    version='0.2',
     description='Interprocess communication framework',
     author='Matthias Gilch',
     author_email='matthias.gilch.mg@gmail.com',
