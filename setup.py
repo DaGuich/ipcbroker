b507c3bea119f7d7dd98496643aebcd54e410690
Matthias Gilch <matthias.gilch.mg@gmail.com>
2017-11-14 12:29:48 +0100

    Update setup.py to 0.2

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     author='Matthias Gilch',
     author_email='matthias.gilch.mg@gmail.com',
     url='https://github.com/DaGuich/ipcbroker',
-    download_url='https://github.com/DaGuich/ipcbroker/archive/0.1.tar.gz',
+    download_url='https://github.com/DaGuich/ipcbroker/archive/0.2.tar.gz',
     keywords=['IPC', 'interprocess', 'communication', 'broker', 'client']
 )
