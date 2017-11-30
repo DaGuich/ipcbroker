770973936ccceb302a9dfd3a0ac22f191f2aed71
Matthias Gilch <matthias.gilch.mg@gmail.com>
2017-11-30 13:27:50 +0100

    Version 0.3

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 setuptools.setup(
     name='ipcbroker',
     packages=['ipcbroker'],
-    version='0.2',
+    version='0.3',
     description='Interprocess communication framework',
     author='Matthias Gilch',
     author_email='matthias.gilch.mg@gmail.com',
     url='https://github.com/DaGuich/ipcbroker',
-    download_url='https://github.com/DaGuich/ipcbroker/archive/0.2.tar.gz',
+    download_url='https://github.com/DaGuich/ipcbroker/archive/0.3.tar.gz',
     keywords=['IPC', 'interprocess', 'communication', 'broker', 'client']
 )
